b0e9dee23e7fc3b6d7e87fa2c3385fd03354b41e
Katherine <kb434@duke.edu>
2022-12-06 17:16:00 -0500

    fixing route

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           {/* This route is for home component 
           with exact path "/", in component props 
           we passes the imported component*/}
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/my-app" element={<Home />} />
             
           {/* This route is for about component 
           with exact path "/about", in component 
